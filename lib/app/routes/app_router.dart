c1fed390d37176534c2be2f6f762d0771a5ff76f
Ifitkhar Ahammad Sikder <eftyshikder111@gmail.com>
2026-02-26 22:31:41 +0600

    added animation to splash screen

--- lib/app/routes/app_router.dart
+++ lib/app/routes/app_router.dart
@@ -1,11 +1,19 @@
 import 'package:go_router/go_router.dart';
+import 'package:zavi_bazaar/features/auth/presentation/screens/sign_in_screen.dart';
 import 'package:zavi_bazaar/features/splash/presentation/screens/splash_screen.dart';
 
-enum Routes { splash, home }
+enum Routes { splash, signIn, home }
 
 class AppRouter {
   static GoRouter router = GoRouter(
     initialLocation: "/",
-    routes: [GoRoute(path: "/", builder: (context, state) => SplashScreen())],
+    routes: [
+      GoRoute(path: "/", builder: (context, state) => SplashScreen()),
+      GoRoute(
+        name: Routes.signIn.name,
+        path: "/signIn",
+        builder: (context, state) => SignInScreen(),
+      ),
+    ],
   );
 }
--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:zavi_bazaar/app/theme/app_text_styles.dart';
 import 'package:zavi_bazaar/core/constants/app_constants.dart';
 import 'package:zavi_bazaar/core/responsive/responsive_helper.dart';
 import 'package:zavi_bazaar/core/ui/dimensions.dart';
 import 'package:zavi_bazaar/gen/assets.gen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _bounceAnimation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(duration: Duration(milliseconds: 800), vsync: this);
+    final animation = CurvedAnimation(parent: _animationController, curve: Curves.easeInOut);
+    _bounceAnimation = Tween<double>(begin: -20.0, end: 20.0).animate(animation);
+    _animationController.repeat(reverse: true);
+
+    if (mounted) {
+      Future.delayed(Duration(seconds: 3), () {
+        if (mounted) context.go('/signIn');
+      });
+    }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +48,16 @@ class SplashScreen extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Assets.images.logo.image(width: 75),
-                  SizedBox(height: Dimensions.paddingSizeDefault),
+                  AnimatedBuilder(
+                    animation: _bounceAnimation,
+                    builder: (context, child) {
+                      return Transform.translate(
+                        offset: Offset(0, _bounceAnimation.value),
+                        child: Center(child: Assets.images.logo.image(width: 70)),
+                      );
+                    },
+                  ),
+                  SizedBox(height: Dimensions.paddingSizeOverLarge),
 
                   Text(
                     AppConstants.appName,
